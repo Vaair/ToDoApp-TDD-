cb1947998e7bb5279b6a843bc8a5754aa1054014
Валерия <valeria.tarasenko.a@gmail.com>
2020-12-22 13:10:21 +0300

    cell configure tests

--- ToDoApp/View/TaskCell.swift
+++ ToDoApp/View/TaskCell.swift
@@ -10,9 +10,23 @@ import UIKit
 class TaskCell: UITableViewCell {
     
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
     
     func configure(withTask task: Task){
+        self.titleLabel.text = task.title
+         
+        let df = DateFormatter()
+        df.dateFormat = "dd.MM.yy"
         
+        if let date = task.date {
+            let dateString = df.string(from: date)
+            dateLabel.text = dateString
+        }
+        
+        if let location = task.location {
+            self.locationLabel.text = location.name
+        }
     }
-
+    
 }
--- ToDoAppTests/View/TaskCellTests.swift
+++ ToDoAppTests/View/TaskCellTests.swift
@@ -35,6 +35,50 @@ class TaskCellTests: XCTestCase {
     func testCellHasTitleLabelInContentView() {
         XCTAssertTrue(cell.titleLabel.isDescendant(of: cell.contentView))
     }
+    
+    func testCellHasLocationLabel() {
+        XCTAssertNotNil(cell.locationLabel)
+    }
+    
+    func testCellHasLocationLabelInContentView() {
+        XCTAssertTrue(cell.locationLabel.isDescendant(of: cell.contentView))
+    }
+    
+    func testCellHasDateLabel() {
+        XCTAssertNotNil(cell.dateLabel)
+    }
+    
+    func testCellHasDateLabelInContentView() {
+        XCTAssertTrue(cell.dateLabel.isDescendant(of: cell.contentView))
+    }
+    
+    func testCongigureSetsTitle() {
+        let task = Task(title: "Foo")
+        
+        cell.configure(withTask: task)
+        
+        XCTAssertEqual(cell.titleLabel.text, task.title)
+    }
+    
+    func testCongigureSetsDate() {
+        let task = Task(title: "Foo")
+        
+        cell.configure(withTask: task)
+        let df = DateFormatter()
+        df.dateFormat = "dd.MM.yy"
+        let date = task.date
+        let dateString = df.string(from: date!)
+        
+        XCTAssertEqual(cell.dateLabel.text, dateString)
+    }
+    
+    func testCongigureSetsLocationName() {
+        let task = Task(title: "Foo", description: nil, location: Location(name: "Bar"))
+        
+        cell.configure(withTask: task)
+        
+        XCTAssertEqual(cell.locationLabel.text, task.location?.name)
+    }
 
 }
 
