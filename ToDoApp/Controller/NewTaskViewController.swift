4c5d31dcdabc00832b78d33a936dd40c0fc9ef2d
Валерия <valeria.tarasenko.a@gmail.com>
2020-12-23 14:00:16 +0300

    Controller for New Tasks

---
+++ ToDoApp/Controller/NewTaskViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NewTaskViewController.swift
+//  ToDoApp
+//
+//  Created by Лера Тарасенко on 23.12.2020.
+//
+
+import UIKit
+
+class NewTaskViewController: UIViewController {
+
+     
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var adressTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+}
