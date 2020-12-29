461c2d7c3b8c59ecedb20dbcf58aa45bb2e98043
Валерия <valeria.tarasenko.a@gmail.com>
2020-12-29 12:37:01 +0300

    AddNewTask button Tests

--- ToDoApp/Controller/TaskListViewController.swift
+++ ToDoApp/Controller/TaskListViewController.swift
@@ -10,14 +10,19 @@ import UIKit
 class TaskListViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
-    
     @IBOutlet var dataProvider: DataProvider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
     }
 
-
+    @IBAction func addNewTask(_ sender: UIBarButtonItem) {
+        if let viewController = storyboard?.instantiateViewController(withIdentifier: String(describing: NewTaskViewController.self)) as? NewTaskViewController {
+            present(viewController, animated: true, completion: nil)
+        }
+    }
+    
 }
 
